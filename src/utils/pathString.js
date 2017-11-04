79ee52883b60ac319647e051feb47ed1501c4178
W. Evan Sheehan <wallace.sheehan@gmail.com>
2017-11-04 11:27:57 -0400

    Remove console statements

--- src/utils/pathString.js
+++ src/utils/pathString.js
@@ -1,9 +1,6 @@
 import {line} from "d3-shape";
 
 export default function pathString(x, y, values) {
-  console.log("x", x.scale.domain(), x.scale.range());
-  console.log("y", y.scale.domain(), y.scale.range());
-  
   return (line()
     .x(d => x.scale(x.value(d)))
     .y(d => y.scale(y.value(d)))(values));
