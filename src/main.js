a43a495762ae26b5eea5b3ac927258a1b37fefea
W. Evan Sheehan <wallace.sheehan@gmail.com>
2017-11-19 12:59:54 -0500

    Remove Svelte
    - Remove Svelte rollup plugin
    - Delete Svelte components
    - Add d3-selection as a dependency

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
-import App from "./App.html";
+import {select} from "d3-selection";
+
+import {simonEhrlichWager} from "./model";
 import prices from "../data/metals.json";
 
-new App({
-  target: document.body.querySelector("main"),
-  data: {
-    startDate: 1980,
-    endDate: 1990,
-    wager: 1000,
-    prices
-  }
-});
+let state = {
+  startYear: 1980,
+  endYear: 1990,
+  initialValue: 1000
+};
+
+let main = select("#root");
+
+main.append("header")
+  .append("h1")
+  .text("Simon-Ehrlich Wager");
+
+function update() {
+  draw(simonEhrlichWager(prices,
+    state.startYear,
+    state.endYear,
+    state.initialValue));
+}
+
+function draw(data) {
+  
+}
+
+update();
